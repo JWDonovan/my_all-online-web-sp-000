82194da102303321b5ceaeb87b9a94d80859caae
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-14 21:14:41 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,14 +1,5 @@
 require 'pry'
 
 def my_all?(collection)
-  i = 0
-  arr = []
-
-  while i < collection.length do
-    ret = yield(collection[i])
-    ret || !ret ? return ret : arr << ret
-    i += 1
-  end
-
-  arr
+  
 end
